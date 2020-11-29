7b4b97262d219dc023f4dcc329dfc093ca91fa2b
Darlyn Norlay <norlaydee@hotmail.co.uk>
2020-11-29 13:44:07 +0000

    Cards trun on click in js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
-    //To flip card to front 
+    //Find all elements with the class name .card
+    const card = document.querySelectorAll(".card");
 
-    $('.card').click(function(){
-        $(this).toggleClass('show-card');
-    
-        
-    });
+    //Set variable for the first and second card click 
+    let firstClick;
+    let secondClick;
 
+    function turnCard() {
+        $('.card').click(function(){
+            $(this).toggleClass('show-card');   
+        });
+    }
     
+    $(turnCard);
+
 });
+
+    
+
+
+
